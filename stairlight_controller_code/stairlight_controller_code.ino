39a75af89c4204a8728a5e9b7fe646d838072479
alphaseneca <alphatheseneca@protonmail.com>
2023-11-22 13:44:19 +0545

    Uploaded the final code

--- stairlight_controller_code/stairlight_controller_code.ino
+++ stairlight_controller_code/stairlight_controller_code.ino
@@ -1,82 +1,96 @@
-  
-  const int lower_pir_pin = 12;   // Pin Configuration
-  const int upper_pir_pin = 14;
-  const int relay_pin = 15;
-  const int ldr_pin = 12;
 
+/* 
+This is the code for the stairlight_controller.
+Author: Ukesh Aryal
+Date Created: 22/11/2023
+*/
 
+const int lower_pir_pin = D0;   // Set the lower_pir_pin
+const int upper_pir_pin = D5;   // Set the upper_pir_pin
+const int relay_pin = D2;       // Set the relay pin
+const int led_pin = D4;         // Set the led pin
+bool lower_pir = 0;             // Initialize all the flag and setup values as 0. 
+bool upper_pir = 0;
+bool lowerflag = 0;
+bool upperflag = 0;
+uint16_t threshold = 45;   // Light threshold that if crossed below activates the system.
+uint16_t hold_time = 20;   // Time for the light to remain on.
 
-  bool lower_pir = 0;
-  bool upper_pir = 0;
-  bool lowerflag = 0 ;
-  bool upperflag = 0 ;
-  uint16_t holdtime = 10  ;      // Delay for the light to remain on. 
-  uint16_t ldrthreshold = 0 ;     // Threshold Value
-  uint16_t resttime = 5000; 
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
-  pinMode(lower_pir_pin, INPUT);
+  Serial.begin(9600);
+  Serial.println("Hello, stairlight_controller!");
+
+  pinMode(lower_pir_pin, INPUT);    // Define all the pin functions.
   pinMode(upper_pir_pin, INPUT);
   pinMode(relay_pin, OUTPUT);
-  digitalWrite(relay_pin, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 
-void readinput(){
+void readinput() {                  // Takes the input from the pins and sets the value of the flag.
 
   lower_pir = digitalRead(lower_pir_pin);
   upper_pir = digitalRead(upper_pir_pin);
 
-  if (lower_pir == 1 && lowerflag == 0 && upperflag == 0){
+  if (lower_pir == 1 && lowerflag == 0 && upperflag == 0) {
     lowerflag = 1;
   }
-
-  if (upper_pir == 1 && lowerflag == 0 && upperflag == 0){
+  if (upper_pir == 1 && lowerflag == 0 && upperflag == 0) {
     upperflag = 1;
   }
-
 }
 
-void stairlight(){
+void stairlight() {                 /*  Checks the flag and if the flag is high then turns off
+                                        the input for the time the light is on.  */
+  if (lowerflag == 1) {
 
-  if (lowerflag == 1){
-
-    Serial.println("Ascending motion detected.");
-        digitalWrite(relay_pin, HIGH);
-    for (int i = 0; i<= holdtime ; i++ ){
-    Serial.println(i);
-    delay(1000);
+    Serial.println("Motion Detected. L_U");
+    digitalWrite(relay_pin, HIGH);
+    for (int i = 0; i <= hold_time; i++) {
+      Serial.println(i);
+      delay(1000);
     }
     Serial.println("Motion Timeout");
-    lowerflag = 0 ;
+    lowerflag = 0;
     digitalWrite(relay_pin, LOW);
   }
 
 
-  else if (upperflag == 1){
-    Serial.println("Descending motion detected.");
-        digitalWrite(relay_pin, HIGH);
-    for (int i = 0; i<= holdtime ; i++ ){
-    Serial.println(i);
-    delay(1000);
+  else if (upperflag == 1) {
+    Serial.println("Motion Detected. U_L");
+    digitalWrite(relay_pin, HIGH);
+    for (int i = 0; i <= hold_time; i++) {
+      Serial.println(i);
+      delay(1000);
     }
     Serial.println("Motion Timeout");
-    upperflag = 0 ;
+    upperflag = 0;
     digitalWrite(relay_pin, LOW);
   }
 
   else {
-      digitalWrite(relay_pin, LOW);
-
+    digitalWrite(relay_pin, LOW);
   }
 }
 
 void loop() {
 
-  readinput();
-  stairlight();
+  digitalWrite(LED_BUILTIN, HIGH);      // Activate the on_board led
+  int ldr_value = analogRead(A0);       // Store the analogvalue read from A0 to the ldr_value.
+
+  Serial.println(ldr_value);            
+  if (ldr_value <= threshold){          // If the ldr_value is less than the threshold then it activates the system.
+    Serial.println("Stairlights Active.");
+    readinput();
+    stairlight();
+  }
+
+  else {
+  digitalWrite(relay_pin, LOW);
+  }
+  delay(1);                              // Delay in between reads for stability
+
+
 
 }
