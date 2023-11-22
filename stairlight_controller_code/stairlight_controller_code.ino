4bda7d6f2334927e3c28939107cce7d2cac5e530
alphaseneca <alphatheseneca@protonmail.com>
2023-11-22 13:45:44 +0545

    Added comments to the code

--- stairlight_controller_code/stairlight_controller_code.ino
+++ stairlight_controller_code/stairlight_controller_code.ino
@@ -5,8 +5,8 @@ Author: Ukesh Aryal
 Date Created: 22/11/2023
 */
 
-const int lower_pir_pin = D0;   // Set the lower_pir_pin
-const int upper_pir_pin = D5;   // Set the upper_pir_pin
+const int lower_pir_pin = D5;   // Set the lower_pir_pin
+const int upper_pir_pin = D0;   // Set the upper_pir_pin
 const int relay_pin = D2;       // Set the relay pin
 const int led_pin = D4;         // Set the led pin
 bool lower_pir = 0;             // Initialize all the flag and setup values as 0. 
@@ -24,7 +24,7 @@ void setup() {
   pinMode(lower_pir_pin, INPUT);    // Define all the pin functions.
   pinMode(upper_pir_pin, INPUT);
   pinMode(relay_pin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(led_pin, OUTPUT);
 }
 
 
@@ -76,12 +76,12 @@ void stairlight() {                 /*  Checks the flag and if the flag is high 
 
 void loop() {
 
-  digitalWrite(LED_BUILTIN, HIGH);      // Activate the on_board led
   int ldr_value = analogRead(A0);       // Store the analogvalue read from A0 to the ldr_value.
 
   Serial.println(ldr_value);            
   if (ldr_value <= threshold){          // If the ldr_value is less than the threshold then it activates the system.
     Serial.println("Stairlights Active.");
+    digitalWrite(led_pin, HIGH);        // Activate the on_board led
     readinput();
     stairlight();
   }
